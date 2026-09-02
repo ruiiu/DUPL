35fe369e800db828f33ea8e875e8660775b60246
Rui Liu <91287176+ruiiu@users.noreply.github.com>
2026-09-01 20:34:00 -0700

    Clean up launch_qwen.sh by removing comments
    Removed commented configuration lines for head and worker node IPs.

--- evaluation/launch_qwen.sh
+++ evaluation/launch_qwen.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # -------- CONFIGURATION --------
-# HEAD_NODE_IP="29.127.68.253"             # Head node IP
-# HEAD_NODE_PORT="6379"
-# Worker node IPs, "29.119.96.254" 29.232.224.137 "29.127.36.241" "29.191.211.78" 29.232.228.185, 29.127.80.107, 29.160.40.86, 29.160.43.142
 
 SSH_USER="root"                        # SSH user for all nodes
 CONDA_ENV="easyr1"                        # Conda environment name
@@ -12,10 +9,6 @@ RAY_GPU_COUNT=8                         # GPUs per node for Ray
 
 # -------- START RAY HEAD --------
 # echo "[HEAD] Starting Ray head node..."
-
-export NCCL_SOCKET_IFNAME=$NETWORK_INTERFACE
-export http_proxy="http://star-proxy.oa.com:3128"
-export https_proxy="http://star-proxy.oa.com:3128"
 
 # pkill -f python 
 # ray stop > /dev/null 2>&1
